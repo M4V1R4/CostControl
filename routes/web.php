9186ea93aff694d2356ae585fbd0a4fd7223b4da
Marco Ramirez <mramirezp@est.utn.ac.cr>
2020-11-29 00:10:23 -0600

    Change composer.json and routes

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('auth/google', 'GoogleController@redirectToProvider')->name('google.login');
+Route::get('auth/google/callback', 'GoogleController@handleProviderCallback');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
