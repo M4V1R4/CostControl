0d8ae6578b0397a17361e631900ae56e98654c73
Marco Ramirez <mramirezp@est.utn.ac.cr>
2020-11-30 22:21:26 -0600

    Change categorias.blade views

--- resources/views/categorias/categorias.blade.php
+++ resources/views/categorias/categorias.blade.php
@@ -1,77 +1,85 @@
-@extends('layouts.app')
-
+@extends('adminlte::page')
+@section('title','Categorias')
+<link href="{{ asset('css/home.css') }}" rel="stylesheet">
 @section('content')
 <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="col-md-20">
-                
-                    
-                <form action="{{ route('categorias.store') }}" method="POST">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Categorías</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{ route('categorias.store') }}" method="POST">
+                        @csrf
+                        <div class="form">
+                            <div class="row">
+                                <div class="col-md-6 form-field">
+                                    <select name="catPadre" id="catPadre" class="input-select">
+                                    <option value="N/A">N/A</option> 
+                                        @foreach($sub as $key => $value )
+                                            <option value="{{ $key }}">{{$key}}</option> 
+                                        @endforeach
+                                    </select>
+                                    <span>Categoría padre</span>
+                                </div>
+                                <div class="col-md-6 form-field">
+                                    <select name='tipo' class="input-select" required>
+                                        <option  value selected disabled hidden></option>
+                                        <option>Ingreso</option>
+                                        <option>Gasto</option>
+                                    </select>
+                                    <span>Tipo</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6 form-field">
+                                <input id="valorDeSelect2" type="text" name="descripcion" class="input-text" required>
+                                    <span>Descripción</span>
+                                </div>
+                                <div class="col-md-6 form-field">
+                                <input name='presupuesto'  type="text" class="input-text" required>
+                                    <span>Presupuesto</span>
+                                </div>
+                            </div>
+                            <div class="col-md-12 ">
+                                    <div class="d-flex justify-content-center">
+                                        <button  type="submit"  id='agregar'  class="btn btn-primary">Agregar</button>
+                                    </div>
+                                </div>
+                        </div>
+                            <!-- <input id="valorDeSelect" type="text" name="catPadre"> -->
+                            <!-- <label for="descripcion" class="col-md-4 col-form-label text-md-right">Descripcion:</label>
+                            <select name="no_conformidad" id="no_conformidad" onchange="selecOp2(event.target.value)">
+                            <option value=""></option> 
+                                @foreach( $sub as $key => $value )
 
-                    @csrf
-                    <div class="input-group">
-                    <label for="nombre" class="col-md-4 col-form-label text-md-right">Tipo:</label> 
-                    <select name='tipo' class="selectpicker" >
-                    <option value=""></option> 
-                            <option>Ingreso</option>
-                            <option>Gasto</option>
-                    </select>
-                    <label for="nombre" class="col-md-4 col-form-label text-md-right">Categoria Padre:</label>                            
-                        <select name="no_conformidad" id="no_conformidad" onchange="selecOp(event.target.value)">
-                        <option value=""></option> 
-                            @foreach( $padre as $key => $value )
+                                    <option value="{{ $key }}">{{ $key}}</option> 
 
-                                <option value="{{ $key }}">{{ $key}}</option> 
-
-                            @endforeach
-                        </select>
-                        
-                        <input id="valorDeSelect" type="text" name="catPadre">
-                        <label for="descripcion" class="col-md-4 col-form-label text-md-right">Descripcion:</label>
-                        <select name="no_conformidad" id="no_conformidad" onchange="selecOp2(event.target.value)">
-                        <option value=""></option> 
-                            @foreach( $sub as $key => $value )
-
-                                <option value="{{ $key }}">{{ $key}}</option> 
-
-                            @endforeach
-                        </select>
-                        <input id="valorDeSelect2" type="text" name="descripcion" value="">
-                        <label for="tasa" class="col-md-4 col-form-label text-md-right">Presupuesto:</label>
-                        <input name='presupuesto'  type="text" class="form-control" aria-label="" >
-                        <div class="from-group mx-sm-4">
-                        <?php /**<label for="imagen">Icono:</label>
-                        <input type="file" class="form-control" name="icono" id="imagen">
-                        */?>
-
-                    </div>
-
-                        
-                        <button  type="submit"  id='agregar'  class="btn btn-primary">Agregar</button>
-                    </div>
-                    </div>
-                </form>
-                
-
-                <hr>
-            
-                <table id='tabla' class="table table-sm table-light">
+                                @endforeach
+                            </select> -->
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-md-12">
+            <div class="table-responsive-md">
+                <table id='tabla' class="table table-striped table-hover content-table text-center">
                     <thead>
                         <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">CatPadre</th>
-                        <th scope="col">Tipo</th>
-                        <th scope="col">Descripcion</th>
-                        <th scope="col">Presupuesto</th>
-                        
-                        
+                            <th scope="col">ID</th>
+                            <th scope="col">CatPadre</th>
+                            <th scope="col">Tipo</th>
+                            <th scope="col">Descripcion</th>
+                            <th scope="col">Presupuesto</th>
+                            <th scope="col">Opciones</th>
                         </tr>
                     </thead>
-
-                   
                         <tbody>
-                     
                             @foreach($categoriasl  as $catego)
                             <tr>
                                 <td name='id'>{{$catego->id }}</td>
@@ -80,51 +88,21 @@
                                 <td name='format'>{{$catego->tipo}}</td>
                                 <td name='format'>{{$catego->descripcion}}</td>
                                 <td name='saldoInicial'>{{$catego->presupuesto}}</td>
-                                
-
                                 <td>
                                     <form method="POST" class="form-delete" action="{{ route('categorias.destroy',$catego->id)}}">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i></button>
-
-                                       
-                           
-                                        <a type="submit"href="{{ route('categorias.edit',$catego->id) }}" class="btn btn-warning" ><i class="fa fa-edit"></i></a>
-                   
-                                       
+                                        <button type="submit" class="btn btn-danger btn-crud"><i class="fa fa-trash"></i></button>
+                                        <a type="submit"href="{{ route('categorias.edit',$catego->id) }}" class="btn btn-success btn-crud" ><i class="fa fa-edit"></i></a>
                                     </form>
                                 </td>
                              </tr>
                             @endforeach
-                
                         </tbody>
                 </table>
             </div>
         </div>
-    <div>
-        
-    <script>
-    var valorEnvio = ""
-
-    function selecOp(valor){
-    document.getElementById("valorDeSelect").value = valor
-    }
-
- 
-    var valorEnvio2 = ""
-
-    function selecOp2(valor){
-    document.getElementById("valorDeSelect2").value = valor
-    }
-
-
-</script>
-
-
-
-
-
-
+    </div>
+<div>
 
 @endsection
