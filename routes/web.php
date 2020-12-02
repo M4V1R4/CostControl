39e2ce6b3e32fcd69148b53325630bd6aa1e856f
Marco Ramirez <mramirezp@est.utn.ac.cr>
2020-12-02 14:01:31 -0600

    Add app_env for use https

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,7 @@ Route::post('/reporte1', 'ReportesController@show1')->name('reporte1.consultar')
 Route::post('/reporte2', 'ReportesController@show2')->name('reporte2.consultar');
 Route::post('/reporte5', 'ReportesController@show5')->name('reporte5.consultar');
 Route::post('/reporte6', 'ReportesController@show6')->name('reporte6.consultar');
+
+if(env('APP_ENV')=== 'production'){
+    \URL::forceScheme('https');
+}
